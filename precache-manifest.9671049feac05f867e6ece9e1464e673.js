18cc0647fe4d1916ad4224cacb01c7c673b78022
mainu007 <mainudev.co@gmail.com>
2020-05-11 12:53:20 +0600

    Updates

---
+++ precache-manifest.9671049feac05f867e6ece9e1464e673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e694d4d7860993dfb6a1fda0bdde2f8",
+    "url": "/shoppingCart/index.html"
+  },
+  {
+    "revision": "4e0a09ea9e4ae8540270",
+    "url": "/shoppingCart/static/css/main.a30142d0.chunk.css"
+  },
+  {
+    "revision": "bdb75a36d7b2e846494a",
+    "url": "/shoppingCart/static/js/2.d9ab022c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shoppingCart/static/js/2.d9ab022c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0a09ea9e4ae8540270",
+    "url": "/shoppingCart/static/js/main.be3f3385.chunk.js"
+  },
+  {
+    "revision": "798c1bab77984bbe9731",
+    "url": "/shoppingCart/static/js/runtime-main.fc330d35.js"
+  }
+]);
